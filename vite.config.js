dc00531eab91f454bf1e63faeae027e1c6891d6e
husham-99 <elmax99@gmail.com>
2025-10-28 04:58:09 -0700

    hello

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/r-weather-app/",
+  loadPath: `${import.meta.env.BASE_URL}locales/{{lng}}/{{ns}}.json`,
   plugins: [react()],
 })
